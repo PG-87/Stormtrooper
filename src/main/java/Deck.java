aca787233eda14cd787853712148139ebeee127f
Tugba A <karaca.tgb@gmail.com>
2019-12-06 13:28:28 +0100

    modified hand class

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -9,19 +9,9 @@ public class Deck {
     public void shuffleDeck(){
         Collections.shuffle(deckOfCards);
     }
-    public void newDeck(){
-        for (int i = 1; i <= 10; i++) {
-            Card card = new Card(HEARTS,i,i);
-            deckOfCards.add(card);
-        }
-        deckOfCards.add(new Card(HEARTS,JACK, 10));
-        deckOfCards.add(new Card(HEARTS,QUEEN, 10));
-        deckOfCards.add(new Card(HEARTS,KING, 10));
-        deckOfCards.add(new Card(HEARTS,ACE, 11));
+    public void addCard( Card card){
+        deckOfCards.add(card);
+    }
 
-    }
-    public void returnCard(Card card){
-        return card;
-    }
 
 }
