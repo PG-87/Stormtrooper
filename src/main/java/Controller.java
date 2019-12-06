aa2a16fe9840e105aa530e4934169a60db519476
sebas <sebastianwaltilla@hotmail.com>
2019-12-06 10:53:41 +0100

    added start

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,2 +1,23 @@
 public class Controller {
+
+
+
+    Controller c;
+
+
+    public void start(Controller Cont){
+            this.c = Cont;
+
+            // släng in programmet här.. dvs
+            // metoder för att köra spelet
+
+
+
+
+
+    }
+
+
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
 public class Main {
+
+
+
+
+
     public static void main(String[] args) {
-        System.out.println("hej");
+
+        Controller controller = new Controller();
+
+
+
+
+
+
+
+
+
     }
 }
