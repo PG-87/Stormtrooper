8c53b973cc914f6b78cc5aedf822ac677f8a62a0
gitPg <pg.patrik@gmail.com>
2019-12-10 10:11:36 +0100

    testing with shit code

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -31,6 +31,7 @@ public class Controller {
         deck.shuffleDeck();         // blanda kortet
         deck.shuffleDeck();         // blanda kortet
         deck.shuffleDeck();         // blanda kortet
+        deck.shuffleDeck();         // blanda kortet
         Deck player = new Deck(); //player kort
         Deck computer = new Deck(); //computer kort
 //-------------------------------
